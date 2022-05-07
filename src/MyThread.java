a8443d5719a2c752052ef88b2ebddf853ef38e0c
Stanislav Rakitov <prosperousrf@gmail.com>
2022-05-07 12:26:59 +0300

    Задача 1. Межпоточный диалог

---
+++ src/MyThread.java
@@ -0,0 +1,23 @@
+/**
+ * @author Stanislav Rakitov
+ */
+class MyThread extends Thread {
+
+  public MyThread(ThreadGroup group, String name) {
+    super(group, name);
+  }
+
+  @Override
+  public void run() {
+    try {
+      while(!isInterrupted()) {
+        Thread.sleep(2500);
+        System.out.printf("Я поток %s. Всем привет!\n", getName());
+      }
+    } catch (InterruptedException err) {
+      System.out.printf("Поток %s прерван.\n", getName());
+    } finally{
+      System.out.printf("%s завершен\n", getName());
+    }
+  }
+}
